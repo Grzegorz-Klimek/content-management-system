6ed85186071e05824f7ac4c653e52a87adebdf27
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-12-05 10:30:15 +0100

    fix with display data

--- index.php
+++ index.php
@@ -1,65 +1,66 @@
-<?php include "includes/header.php" ?>
-<?php include "includes/db.php" ?>
 
-<!-- Navigation -->
-<?php include "includes/nav.php" ?>
+    <?php include "includes/header.php" ?>
+    <?php include "includes/db.php" ?>
+
+    <!-- Navigation -->
+    <?php include "includes/nav.php" ?>
 
 
-<!-- Page Content -->
-<div class="container">
+    <!-- Page Content -->
+    <div class="container">
 
-    <div class="row">
+        <div class="row">
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
 
-            <?php
+                <?php
 
-                $query = "SELECT * FROM posts";
+                        $query_posts = "SELECT * FROM posts";
 
-                $select_all_posts_query = mysqli_query($connection, $query);
+                        $select_all_posts_query = mysqli_query($connection, $query_posts);
 
-                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
-                }
-            ?>
+                        while($row = mysqli_fetch_assoc($select_all_posts_query)) {
 
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
+                ?>
 
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Simple CMS</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus
-                inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis
-                ipsum officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span
-                        class="glyphicon glyphicon-chevron-right"></span></a>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
 
-            <hr>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus
+                    inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis
+                    ipsum officiis rerum.</p>
+                <a class="btn btn-primary" href="#">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
 
+                <hr>
+                <?php } ?>
+s
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/sidebar.php" ?>
 
         </div>
+        <!-- /.row -->
 
-        <!-- Blog Sidebar Widgets Column -->
-        <?php include "includes/sidebar.php" ?>
-
-    </div>
-    <!-- /.row -->
-
-    <hr>
+        <hr>
 <?php include "includes/footer.php"; ?>
